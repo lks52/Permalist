5d02821a23fabbf9cf2384589c16618bdc3b62d7
Lks5200 <lukasschmidt00@gmail.com>
2024-06-20 09:21:03 +0200

    Implemet all the functionality

--- 8.6 Permalist Project/index.js
+++ 8.6 Permalist Project/index.js
@@ -1,8 +1,19 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg"
+import configDb from './config/config.js';
 
 const app = express();
 const port = 3000;
+
+const db = new pg.Client({
+    user: "postgres",
+    host: "localhost",
+    database: "Permalist",
+    password: configDb.password,
+    port: 5432,
+});
+db.connect();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
@@ -12,22 +23,69 @@ let items = [
   { id: 2, title: "Finish homework" },
 ];
 
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
+  try {
+    await getItems();
   res.render("index.ejs", {
     listTitle: "Today",
     listItems: items,
   });
+  } catch (err) {
+    console.log(err)
+  }
 });
 
-app.post("/add", (req, res) => {
-  const item = req.body.newItem;
-  items.push({ title: item });
-  res.redirect("/");
+app.post("/add", async (req, res) => {
+  try {
+    const item = req.body.newItem;
+    await addItem(item)
+    res.redirect("/");
+  } catch (err) {
+    console.log(err)
+  }
 });
 
-app.post("/edit", (req, res) => {});
+app.post("/edit", async (req, res) => {
+  try {
+    let editedItem = req.body.updatedItemTitle
+    let editedId = req.body.updatedItemId
+    await editItem(editedItem, editedId)
+    res.redirect("/")
+  } catch (err) {
+    console.log(err)
+  }
+});
 
-app.post("/delete", (req, res) => {});
+app.post("/delete", async (req, res) => {
+  try {
+    let deleteItemId = req.body.deleteItemId
+    await deleteItem(deleteItemId)
+    res.redirect("/")
+  } catch (err) {
+    console.log(err)
+  }
+});
+
+
+
+async function getItems () {
+  const result = await db.query("SELECT * FROM items ORDER BY id ASC")
+  items = result.rows
+  return items
+}
+
+async function addItem (newItem) {
+  await db.query("INSERT INTO items (title) VALUES ($1)",[newItem])
+  }
+
+async  function editItem (editedTitle, editedId){
+  await db.query("UPDATE items SET title = $1 WHERE id = $2", [editedTitle, editedId])
+}
+
+async  function deleteItem (deliteId){
+  await db.query("DELETE FROM items WHERE id = $1", [deliteId])
+}
+
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
